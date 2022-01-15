ef73e3023ed6babdf1586ff7010fbb7c1153f0b4
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-01-15 10:47:54 +0300

    Found end dir

--- ac.py
+++ ac.py
@@ -8,8 +8,21 @@ ASSETSROOTDIRS = [
 
 
 def parse_folders(path):
-    for subpath in glob.glob(str(path) + os.sep + "\\*", recursive=False):
-        print(subpath)
+    def is_end_dir(files_and_dirs_list):
+        for file_or_dir in files_and_dirs_list:
+            if pathlib.Path(file_or_dir).is_dir():
+                return False
+        return True
+
+    files_and_dirs = glob.glob(str(path) + os.sep + "\\*", recursive=False)
+
+    if is_end_dir(files_and_dirs):
+        print(files_and_dirs)
+        exit()
+    else:
+        for file_or_dir in files_and_dirs:
+            if pathlib.Path(file_or_dir).is_dir():
+                parse_folders(file_or_dir)
 
 
 for root_dir in ASSETSROOTDIRS:
