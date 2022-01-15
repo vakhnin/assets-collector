9687fbb0718a4bd518c4f99058f5ab0f041eddf6
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-01-15 10:23:56 +0300

    Enumeration root folder

--- ac.py
+++ ac.py
@@ -1,8 +1,17 @@
+import glob
 import pathlib
 
 ASSETSROOTDIRS = [
     pathlib.Path(r'd:\Google Диск\LinePoets\Works\(A-F)'),
 ]
 
+
+def parse_folders(path):
+    # for subpath in path.glob("**"):
+    #     print(subpath)
+    for subpath in glob.glob(r'd:\Google Диск\LinePoets\Works\(A-F)\*', recursive=False):
+        print(subpath)
+
+
 for root_dir in ASSETSROOTDIRS:
-    print(root_dir)
+    parse_folders(root_dir)
