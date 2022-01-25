81de39eaa2f0899a8ba63cacfc5ebfbcc5e418f9
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-01-25 15:35:11 +0300

    Make layout result html

--- html/html.py
+++ html/html.py
@@ -6,32 +6,34 @@ TOPHTML = '''<!doctype html>
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <link rel="stylesheet" href="../html/ac.css">
-    <title>Document</title>
+    <title>LinePoets works</title>
 </head>
 <body>    
 
+    <h1>LinePoets works</h1>
+
     <table>
         <tr>
-            <th>№</th>
-            <th>Thumbnail</th>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Full path</th>
+            <th><div>№</div></th>
+            <th><div>Thumbnail</div></th>
+            <th><div>Name</div></th>
+            <th><div>Date</div></th>
+            <th><div>Full path</div></th>
         </tr>
 
 '''
 
 CONTENTHTML = '''
         <tr{}>
-            <td>{}</td>
+            <td><div>{}</div></td>
             <td>
                 <div class="jpg">
                     <img src="img/{}.jpg" alt="">
                 </div>
             </td>
-            <td>{}</td>
-            <td>{}</td>
-            <td>{}</td>
+            <td><div>{}</div></td>
+            <td><div>{}</div></td>
+            <td><div>{}</div></td>
         </tr>
 '''
 
